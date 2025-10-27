c4fda9c33bd62bd27e4bd42132902d1dcf242b70
Amanullah Khan <thabraealam20674@gmail.com>
2025-10-27 20:01:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./Pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
+import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />}></Route>
+      <Route path="tracking" element={<TrackingPage />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
